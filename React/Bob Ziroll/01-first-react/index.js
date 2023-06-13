a2d09fcdc7fd75e4321f209a8d20356cdab930ac
Noah Rubin <noahrubininv@gmail.com>
2023-06-13 15:42:26 +1000

    react initial commit

---
+++ React/Bob Ziroll/01-first-react/index.js
@@ -0,0 +1,15 @@
+ReactDOM.render(
+    <h1>Hello Everyone</h1>, 
+    document.querySelector('#root')
+)
+
+// Exercise: 
+// Render a ul with two li's inside
+ReactDOM.render(
+    <ul>
+        <li>Sydney</li>
+        <li>Melbourne</li>
+        <li>Perth</li>
+    </ul>, 
+    document.querySelector('.cities-container')
+)
