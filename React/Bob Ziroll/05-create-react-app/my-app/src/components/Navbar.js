3093c6a7313144c861f9d6962c9e9aa0cff6901d
Noah Rubin <noahrubininv@gmail.com>
2023-06-16 11:32:58 +1000

    react draft proj

---
+++ React/Bob Ziroll/05-create-react-app/my-app/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h3 class="nav--logo-text">React Facts</h3>
+            <h4 class="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
