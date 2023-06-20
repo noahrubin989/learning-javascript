40d82534e9495f71f025ade37fb086426093e13a
Noah Rubin <noahrubininv@gmail.com>
2023-06-20 20:31:13 +1000

    created card component

--- React/Bob Ziroll/07-airbnb-proj/airbnb/src/components/Card.js
+++ React/Bob Ziroll/07-airbnb-proj/airbnb/src/components/Card.js
@@ -1,22 +1,23 @@
 import React from "react";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+library.add(faStar)
+
 
 export default function Card() {
     return (
-        <section className="experience--instance d-flex flex-column">
-            <div className="experience--image-container"><img src={require("../images/wedding.jpg")} alt="wedding"/></div>
-            <div className="experience--reviews-container d-flex flex-row">
-                {/* <FontAwesomeIcon icon="fa-solid fa-star" style={{color: "#ff0000",}} /> */}
-                <p>5.0</p>
-                <p>(5)-USA</p>
+        <div className="card border-0">
+            <img className="card--image" src={require("../images/wedding.jpg")} alt="wedding"/>
+            <div className="card--stats">
+                <FontAwesomeIcon className="card--star" icon="fa-solid fa-star" style={{color: "#ff0000",}} />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
             </div>
-            <div>
-                <p>Learn wedding photography</p>
-                <p><b>From $125</b> / person</p>
-            </div>
-        </section>
-    )
+            <p className="mb-2 mt-2">Learn wedding photography</p>
+            <span><b>From $125</b> / person</span>
+        </div>
+    );
 }
-
-// Todo
-// Import font awesome icon
