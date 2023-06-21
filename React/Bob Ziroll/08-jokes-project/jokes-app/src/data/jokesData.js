c6db30031cb1970a27b23c2e456df3e92cf3f6fe
Noah Rubin <noahrubininv@gmail.com>
2023-06-22 09:12:59 +1000

    rendering elements using map method

---
+++ React/Bob Ziroll/08-jokes-project/jokes-app/src/data/jokesData.js
@@ -0,0 +1,16 @@
+const jokesArr = [
+    {
+        setup: "Why did the chicken cross the road?",
+        punchLine: "To get to the other side"
+    },
+    {
+        setup: "What kind of candy do astronauts like?",
+        punchLine: "Mars Bars"
+    },
+    {
+        setup: "I am so good at sleeping I can do it with my eyes closed",
+        punchLine: ""
+    }
+];
+
+export default jokesArr;
