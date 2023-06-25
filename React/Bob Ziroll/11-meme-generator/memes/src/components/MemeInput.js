538cab2c5d4450c1a4def995635aaf5f313db940
Noah Rubin <noahrubininv@gmail.com>
2023-06-25 16:56:16 +1000

    arrays and state

--- React/Bob Ziroll/11-meme-generator/memes/src/components/MemeInput.js
+++ React/Bob Ziroll/11-meme-generator/memes/src/components/MemeInput.js
@@ -3,22 +3,16 @@ import memesData from "../data/memesData";
 
 const MemeInput = (props) => {
 
-    function handleClick() {
-        let memesArray = memesData.data.memes;
-        let randomNumber = Math.floor(Math.random() * memesArray.length)
-        console.log(memesArray[randomNumber].url);
-    }
+    let [url, setUrl] = useState('');
 
-    const [thingsArray, setThingsArray] = useState(['Thing 1', 'Thing 2']);
-
-    function addThing() {
-        setThingsArray(prevArray => [...prevArray, `Thing ${prevArray.length + 1}`]);
+    function getImg() {
+        setUrl(()=>{
+            const memesArray = memesData.data.memes;
+            const randomNum = Math.floor(Math.random()*memesArray.length);
+            return memesArray[randomNum].url;
+        })
     }
-    const pElements = thingsArray.map(
-        (item, index)=>{
-            return <p key={index}>{item}</p>;
-        }
-    )
+    
 
     return (
         <section className="meme-generation-container">
@@ -27,14 +21,9 @@ const MemeInput = (props) => {
                 <input type="text" className="bottom-text" placeholder="bottom-text"/>
             </form>
             <div className="d-grid">
-                <button onClick={handleClick} className="btn btn-dark" type="button">Get a new meme image</button>
+                <button onClick={getImg} className="btn btn-dark" type="button">Get a new meme image</button>
             </div>
-            <div>
-                <button onClick={addThing} className="btn btn-dark" type="button">
-                    Add item
-                </button>
-                {pElements}
-            </div>
+            <img className="meme-image" src={url} alt="meme"/>
         </section>
     );
 }
