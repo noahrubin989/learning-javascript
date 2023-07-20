d06b4b430c04e054e0003db8a99b001acab16ca7
noahrubin989 <noahrubininv@gmail.com>
2023-07-21 09:52:23 +1000

    changes

--- React/Bob Ziroll/19-window-tracker/src/App.js
+++ React/Bob Ziroll/19-window-tracker/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(
+    // This will execute every time our app renders
+    () => {
+      fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+        .then((r) => r.json())
+        .then((json) => setItems(json));
+    },
+    // values such that whenever they change, the code inside your hook is going to run,
+    [resourceType]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h1>Window width = {windowWidth}</h1>
+      <hr />
+      <div>
+        <button
+          onClick={() => {
+            setResourceType("posts");
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Posts
+        </button>
+        <button
+          onClick={() => {
+            setResourceType("users");
+          }}
+        >
+          Users
+        </button>
+        <button
+          onClick={() => {
+            setResourceType("comments");
+          }}
+        >
+          Comments
+        </button>
+        <h1>{resourceType}</h1>
+        {items.map((item) => {
+          return <pre>{JSON.stringify(item)}</pre>;
+        })}
+      </div>
+    </>
   );
 }
 
